5f310f71656948783d2416d16c0edf79f81a8572
frostpyro2 <143861009+frostpyro2@users.noreply.github.com>
2025-09-12 18:52:20 +0900

    Manage.java 업데이트

--- src/main/java/org/example/contentplugin/rpgcore/damage/data/Manage.java
+++ src/main/java/org/example/contentplugin/rpgcore/damage/data/Manage.java
@@ -6,7 +6,7 @@ import org.example.contentplugin.rpgcore.damage.data.damage.AttackData;
 
 public class Manage {
     public AttackData findAttack(EntityDamageEvent event){
-LivingEntity target = event.getEntity()
+        LivingEntity target = event.getEntity()
         if(event instanceof EntityDamageByEntityEvent byEntity){
 
         }
